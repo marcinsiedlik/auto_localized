8f8e209cafd98678cd436aaa92fe34520cd7ccd3
Marcin Siedlik <marcin.siedlik97@gmail.com>
2020-08-03 22:28:23 +0200

    Create localized string, add export

--- packages/auto_localized/lib/auto_localized.dart
+++ packages/auto_localized/lib/auto_localized.dart
@@ -2,4 +2,5 @@ library auto_localized;
 
 export 'package:auto_localized/src/localization/auto_localization.dart';
 export 'package:auto_localized/src/localization/auto_localization_delegate.dart';
+export 'package:auto_localized/src/string/localized_string.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
