6726228dbef089e656239724e9a516763ee07925
Marcin Siedlik <marcin.siedlik97@gmail.com>
2020-08-09 11:47:59 +0200

    Create locale info

---
+++ packages/auto_localized_generator/lib/src/model/locale_info.dart
@@ -0,0 +1,20 @@
+class LocaleInfo {
+  final String languageCode;
+  final String countryCode;
+
+  const LocaleInfo(this.languageCode, this.countryCode) : assert(languageCode != null);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is LocaleInfo &&
+          runtimeType == other.runtimeType &&
+          languageCode == other.languageCode &&
+          countryCode == other.countryCode;
+
+  @override
+  int get hashCode => languageCode.hashCode ^ countryCode.hashCode;
+
+  @override
+  String toString() => languageCode + (countryCode != null ? '_$countryCode' : '');
+}
