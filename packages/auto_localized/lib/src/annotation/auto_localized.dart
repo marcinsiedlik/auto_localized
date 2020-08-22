cced9de135bcb8cc705ece948ae85cab19ad0038
Marcin Siedlik <marcin.siedlik97@gmail.com>
2020-08-22 13:17:06 +0200

    Add documentation comments

--- packages/auto_localized/lib/src/annotation/auto_localized.dart
+++ packages/auto_localized/lib/src/annotation/auto_localized.dart
@@ -3,14 +3,53 @@
 import 'package:auto_localized/src/annotation/auto_localized_locale.dart';
 import 'package:auto_localized/src/annotation/on_blank_value_strategy.dart';
 
+///AutoLocalized generator config annotation class
 class AutoLocalized {
+  ///List of [AutoLocalizedLocale], which is the
+  ///main part of the config. List must by not-null and can't be empty.
   final List<AutoLocalizedLocale> locales;
+
+  ///If set to `true` then any key case will be converted to camel case
+  ///in generated source. For example json key:
+  ///```json
+  ///{
+  ///  "test_message": "This is a test message in English"
+  ///}
+  ///```
+  ///
+  ///will be generated to source:
+  ///```dart
+  ///static const testMessage = LocalizedString(
+  ///    key: 'test_message',
+  ///    values: {
+  ///     'en_US': '''This is a test message in English''',
+  ///     'pl': '''To jest testowa wiadomość w języku Polskim''',
+  ///    },
+  ///  );
+  ///```
+  ///
+  /// If set to `false` the original key will be used.
   final bool convertToCamelCase;
+
+  ///Defines the behaviour when the value for key is blank
+  ///or consists of the characters of whites only.
+  ///Blank value will occur in any of the following cases:
+  ///
+  /// ```json
+  ///{
+  ///  "blank_value": "",
+  ///  "also_blank_value": "   ",
+  ///}
+  ///```
+  ///
+  /// This behaviour is will not be triggered if value
+  /// is defined as `null`. In that case generator will
+  /// always throw an [Exception]
   final OnBlankValueStrategy onBlankValueStrategy;
 
   const AutoLocalized({
     this.locales,
-    this.convertToCamelCase = false,
+    this.convertToCamelCase = true,
     this.onBlankValueStrategy = OnBlankValueStrategy.error,
   }) : assert(locales != null);
 }
--- packages/auto_localized/lib/src/annotation/auto_localized_locale.dart
+++ packages/auto_localized/lib/src/annotation/auto_localized_locale.dart
@@ -1,8 +1,16 @@
 // ignore_for_file: always_require_non_null_named_parameters
 
 class AutoLocalizedLocale {
+  ///Language code that will be used in generated source
+  ///Must be not-null and can't be blank
   final String languageCode;
+
+  ///Country code that will be used in generated source
+  ///This field is optional
   final String countryCode;
+
+  ///Path to JSON file that will be parsed during source generation.
+  ///Must be not-null and can't be blank
   final String jsonFilePath;
 
   const AutoLocalizedLocale({
