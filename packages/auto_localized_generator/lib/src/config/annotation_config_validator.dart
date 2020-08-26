ef7986afaeafa95016e69792233cd7e254717882
Marcin Siedlik <marcin.siedlik97@gmail.com>
2020-08-26 22:30:16 +0200

    Create args exceptions

--- packages/auto_localized_generator/lib/src/config/annotation_config_validator.dart
+++ packages/auto_localized_generator/lib/src/config/annotation_config_validator.dart
@@ -2,6 +2,7 @@ import 'package:auto_localized/annotations.dart';
 import 'package:auto_localized_generator/src/config/model/annotation_config.dart';
 import 'package:auto_localized_generator/src/config/model/annotation_config_locale.dart';
 import 'package:auto_localized_generator/src/exception/missing_translation_key_exception.dart';
+import 'package:auto_localized_generator/src/exception/translation_args_bad_order_exception.dart';
 import 'package:auto_localized_generator/src/exception/translation_value_blank_exception.dart';
 import 'package:auto_localized_generator/src/utils/logging.dart';
 import 'package:auto_localized_generator/src/utils/string_extensions.dart';
@@ -12,6 +13,7 @@ class AnnotationConfigValidator {
   void validate(AnnotationConfig config) {
     _assureKeysEqualityInLocales(config.locales);
     _checkForBlankValues(config);
+    _assureArgsCorrectOrderInLocales(config.locales);
   }
 
   void _assureKeysEqualityInLocales(List<AnnotationConfigLocale> locales) {
@@ -55,10 +57,32 @@ class AnnotationConfigValidator {
           throw TranslationValueBlankException(locale, key);
         } else {
           Logger.warning(
-            'Translation value for key: "$key" is blank.\n'
-            'Found in JSON file for locale: "${locale.info.toString()}""',
+            'Translation value for key: "$key" is blank or whitespaces only\n'
+            'Found in translation file for locale: "${locale.info.toString()}""',
           );
         }
+      }
+    });
+  }
+
+  void _assureArgsCorrectOrderInLocales(List<AnnotationConfigLocale> locales) {
+    locales.forEach((locale) {
+      _validateArgsOrder(locale);
+    });
+  }
+
+  void _validateArgsOrder(AnnotationConfigLocale locale) {
+    const _args1 = ['{1}'];
+    const _args2 = ['{1}', '{2}'];
+    const _args3 = ['{1}', '{2}', '{3}'];
+    const _args4 = ['{1}', '{2}', '{3}', '{4}'];
+
+    locale.translations.forEach((key, value) {
+      if ((value.contains('{5}') && !value.containsAll(_args4)) ||
+          (value.contains('{4}') && !value.containsAll(_args3)) ||
+          (value.contains('{3}') && !value.containsAll(_args2)) ||
+          (value.contains('{2}') && !value.containsAll(_args1))) {
+        throw TranslationArgsBadOrderException(locale.info, key, value);
       }
     });
   }
