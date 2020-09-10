0f9e2cbe7b3c237ab678aabc6007fc9284228f61
Marcin Siedlik <marcin.siedlik97@gmail.com>
2020-09-10 16:42:41 +0200

    Refactor AutoLocalization with delegate to singleton

--- packages/auto_localized/lib/src/localization/auto_localization.dart
+++ packages/auto_localized/lib/src/localization/auto_localization.dart
@@ -1,32 +1,37 @@
+// ignore_for_file: prefer_constructors_over_static_methods
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 typedef LocaleUpdateCallback = void Function(Locale locale);
 
 class AutoLocalization {
-  final Locale locale;
+  final List<LocaleUpdateCallback> _onLocaleUpdateCallbacks = [];
 
-  AutoLocalization(this.locale) {
-    lastCachedLocale = locale;
-    _onLocaleUpdateCallbacks.forEach((callback) => callback.call(locale));
+  ///Current app Locale
+  Locale locale;
+
+  static AutoLocalization _instance;
+
+  static AutoLocalization get instance => _instance ?? (_instance = AutoLocalization._());
+
+  AutoLocalization._();
+
+  ///Updates locale, invokes locale change callbacks
+  static void updateLocale(Locale locale) {
+    instance.locale = locale;
+    instance._onLocaleUpdateCallbacks.forEach((element) => element.call(locale));
   }
-
-  ///Updated when [AutoLocalization] constructor is called - locale changed.
-  ///Used for translation without [BuildContext] (experimental).
-  ///Field initializes with null, use with null checks.
-  static Locale lastCachedLocale;
-
-  static final List<LocaleUpdateCallback> _onLocaleUpdateCallbacks = [];
 
   ///Adds the callback with updated [Locale] when [AutoLocalization]
   ///constructor is called.
   static void addOnLocaleChangeCallback(LocaleUpdateCallback onChange) {
-    _onLocaleUpdateCallbacks.add(onChange);
+    instance._onLocaleUpdateCallbacks.add(onChange);
   }
 
   ///Removes callback added in [addOnLocaleChangeCallback].
   static void removeOnLocaleChangeCallback(LocaleUpdateCallback onChange) {
-    _onLocaleUpdateCallbacks.remove(onChange);
+    instance._onLocaleUpdateCallbacks.remove(onChange);
   }
 
   static AutoLocalization of(BuildContext context) => Localizations.of<AutoLocalization>(context, AutoLocalization);
